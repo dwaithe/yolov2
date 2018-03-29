be0b1cc29614394c09ed4aa8ddd1df1f47457805
Dominic Waithe <dominic_waithe@hotmail.com>
2018-03-29 16:44:08 +0100

    process print list

--- scripts/reval_final_list.py
+++ scripts/reval_final_list.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
     iterations = args.iterations[0]
     b = pickle.load(open('results/cell - neuroblastoma phalloidin_'+iterations+'_pr.pkl','rb'))
 
-    print('iterations\t'+b[ap])
+    print('iterations\t'+b['ap'])
 
     
